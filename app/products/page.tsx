5261725a0bda893abb87fddd3d38cb1646d30dd8
Razshy <88072010+Razshy@users.noreply.github.com>
2024-08-04 14:40:08 -0500

    mainpush12
    gg

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,14 @@
-
+import React, { Suspense } from 'react';
 import ProductPage from '@/components/ProductPage';
+
+const Loading = () => <div>Loading...</div>;
+
 function App() {
   return (
     <div className="App">
-      <ProductPage/>
+      <Suspense fallback={<Loading />}>
+        <ProductPage />
+      </Suspense>
     </div>
   );
 }
